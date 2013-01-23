82e860425d8d6e31bfacb61eaec88d3a39647488
Justin Filip <jfilip@remote-learner.ca>
2013-01-23 11:21:07 -0500

    Initial commit of jQuery 1.6.3 for Moodle 2.3

---
+++ lang/en/local_rljquery.php
@@ -0,0 +1,26 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Langauge file for the Remote-Learner jQuery local plugin
+ *
+ * @package    local_rljquery
+ * @link       http://jquery.com/
+ * @author     Remote-Learner.net Inc
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  (C) 2013 Remote Learner.net Inc http://www.remote-learner.net
+ */
+
+$string['pluginname'] = 'Remote-Learner jQuery library';
