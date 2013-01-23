9fb70f6b931fa3093f3a4fbc8eece29ac56706f7
Justin Filip <jfilip@remote-learner.ca>
2013-01-23 11:22:56 -0500

    Initial commit of jQuery 1.6.3 for Moodle 2.4

---
+++ version.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * File containing the version information for this plugin.
+ *
+ * @package    local_rljquery
+ * @link       http://jquery.com/
+ * @author     Remote-Learner.net Inc
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 of later
+ * @copyright  (C) 2013 Remote Learner.net Inc http://www.remote-learner.net
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2011090100; // Release date of this version of jQuery.
+$plugin_requires   = 2012120300.00;
+$plugin->component = 'local_rljquery';
+$plugin->maturity  = MATURITY_BETA;
+$plugin->release   = '1.6.3 (2.4)' // The jQuery release version with the Moodle release in parentheses
