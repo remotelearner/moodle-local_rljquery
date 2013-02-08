915e74d11a8cd8df702911844c860d27290a88dc
Justin Filip <jfilip@remote-learner.ca>
2013-02-08 12:08:16 -0500

    jQuery 1.9.0 for Moodle 2.2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012111300; // Release date of this version of jQuery.
+$plugin->version   = 2013011500; // Release date of this version of jQuery.
 $plugin->requires  = 2011120500.00;
 $plugin->component = 'local_rljquery';
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.8.3 (2.2)'; // The jQuery release version with the Moodle release in parentheses
+$plugin->release   = '1.9.0 (2.2)'; // The jQuery release version with the Moodle release in parentheses
